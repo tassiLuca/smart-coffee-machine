fa23be8abd93cb1a690ca0ed9054c270cf62ee96
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-04-23 11:55:23 +0200

    config project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define PIN_LED 13
+
+void setup() {
+    pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(PIN_LED, HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED, LOW);
+    delay(1000);
+}
