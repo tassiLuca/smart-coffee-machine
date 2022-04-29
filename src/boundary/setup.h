b9e6800f4c622b139e5920c88488cb2530834dde
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-04-29 10:06:06 +0200

    moved into boundary

---
+++ src/boundary/setup.h
@@ -0,0 +1,34 @@
+#ifndef __SETUP__
+#define __SETUP__
+
+// Button pins.
+#define UP_BTN   2
+#define DOWN_BTN 3
+#define MAKE_BTN 4
+
+// Sonar sensor pins.
+#define SONAR_TRIGGER_PIN 8
+#define SONAR_ECHO_PIN    9
+
+// Potentiometer pin.
+#define POTENTIOMETER_PIN A0
+
+// Temperature sensor pin.
+#define TEMPERATURE_SENSOR_PIN A1
+
+// Display infos.
+// Keep in mind, since we are using Arduino Uno:
+// +-------------+-------------+
+// + Display Pin + Arduino Pin +
+// +-------------+-------------+
+// +     SCL     +      A5     +
+// +     SDA     +      A4     +
+// +-------------+-------------+
+// Refer to [https://www.arduino.cc/en/reference/wire] 
+#define DISPLAY_COLS 20
+#define DISPLAY_ROWS 2
+
+// Servo motor.
+#define SERVO_MOTOR_PIN 11
+
+#endif
