c32831643e1a08c49772ee91f0a1fe7720eca2d9
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-04-29 16:34:09 +0200

    doc

--- src/tasks/Task.h
+++ src/tasks/Task.h
@@ -2,8 +2,6 @@
 #define __TASK__
 
 #include "states/State.h"
-#include "../model/Machine.h"
-#include "../boundary/CoffeeMachine.h"
 
 class State;
 
@@ -14,19 +12,31 @@ class Task {
 
 public:
 
+    /**
+     * @brief Initialize the task state. 
+     * @param state the initial state of the task
+     */
     virtual void init(State* state) = 0;
 
+    /**
+     * @brief Execute the task.
+     */
     virtual void execute() = 0;
 
+    /**
+     * @brief Change the current state of the task. 
+     * @param nextState the pointer to the next state.
+     */
     virtual void stateTransition(State* nextState) = 0;
 
+    /**
+     * @brief Updates the elapsed time and check if it's time 
+     * to execute the current task, accordingly with its period. 
+     * @param basePeriod the period of the scheduler
+     * @return true if this task must be executed, else otherwise.
+     */
     virtual bool updateAndCheckTime(const int basePeriod) = 0;
-
-    virtual Machine* getMachine() = 0;
-
-    virtual CoffeeMachine* getSensors() = 0;
-
-    
+  
 };
 
 #endif
