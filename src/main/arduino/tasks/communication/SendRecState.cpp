4f5574c2b7a582ddcafa8550824da7ec8512ec72
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-05-08 18:42:13 +0200

    assistance => ready

--- src/main/arduino/tasks/communication/SendRecState.cpp
+++ src/main/arduino/tasks/communication/SendRecState.cpp
@@ -4,4 +4,11 @@
 
 void SendRecState::handle() {
     MsgService.sendMsg(getMachine()->getInfos());
+    if (MsgService.isMsgAvailable()) {
+        Msg* msg = MsgService.receiveMsg();
+        if (getMachine()->getMachineState() == ASSISTANCE && 
+            (msg->getContent() == "RECOVER" || msg->getContent() == "REFILL")) {
+            getMachine()->setMachineState(READY);
+        }
+    }
 }
